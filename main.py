cc055b70a3c3d0ad08c17e2b7275fca857e115cc
nishanthegde <nishant.arun.hegde@gmail.com>
2025-10-14 16:03:10 -0700

    Update main.py to test trigger

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 
 app = Flask(__name__)
 client = bigquery.Client()
-
 @app.route('/')
+
 def main(big_query_client=client):
     table_id = "udemy-mlops-474821.test_schema.us_states"
     job_config = bigquery.LoadJobConfig(
